3db8a36f553079b1ef4f841777c89179eec7df97
zhaoyingjun <zhaoyingjun@appledeMacBook-Air.local>
2018-12-06 23:49:28 +0800

    fix

--- lessonThree/Anti-Fraud-App/app.py
+++ lessonThree/Anti-Fraud-App/app.py
@@ -17,16 +17,17 @@ import VAE
 app = Flask(__name__) 
 #路由注解，我们这里使用的是path的形式进行传参
 #示例url:http://0.0.0.0:8088/predict/1/2/3/4/5/6/7/8/9/10/11/12 这里的1...12换成需要进行聚类的值就可以了
-@app.route('/predict/<a>/<b>/<c>/<d>/<e>/<f>/<g>/<h>/<i>/<j>/<k>/<l>', methods=['GET'])
-def predict(a,b,c,d,e,f,g,h,i,j,k,l):
+@app.route('/predict/<a>/<b>/<c>/<d>/<e>/<f>/<g>/<h>', methods=['GET'])
+def predict(a,b,c,d,e,f,g,h):
 	#获取url传来的需要进行预测的数据
-	line=[a,b,c,d,e,f,g,h,i,j,k,l]
+	line=[a,b,c,d,e,f,g,h]
     
 	k=len(line)
 	lines=range(k)
-	lines=[int(i) for i in line]
+	lines=[float(i) for i in line]
 	lines=[lines]
 	lines=VAE.vae_encoder(VAE.sess,lines)
+	print(lines)
 	predict_result=execute.predicts(lines)
 
 	return jsonify( { 'result of cluster': str(predict_result) } )
